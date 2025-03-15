e788cfa1275ae9ba4d01f895a83c89299d82470c
burrito <singhsanskar240@gmail.com>
2025-03-16 01:03:07 +0545

    Task 1 done on my end. Waiting for feedback

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const NavElements = ["Home", "Product Catalog", "Contact"];
 const Navbar = () => {
   return (
     <header className='w-full z-10 mb-10 border-b border-gray-100'>
-      <nav className='max-w-[1200px] h-auto mt-6 mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
+      <nav className='flex max-w-[1200px] h-auto mt-6 mx-auto justify-between items-center md:px-16 px-6 py-4 bg-transparent'>
         <Link href='/' className='flex justify-center items-center'>
           {/* <Image
             src='/logo.png'
@@ -18,7 +18,7 @@ const Navbar = () => {
           /> */}
           <h1>Logo</h1>
         </Link>
-        <ul className='sm:flex cursor-pointer list-none items-center gap-x-10'>
+        <ul className='flex justify-center cursor-pointer list-none items-center gap-x-10'>
           {NavElements.map((item, _id) => (
             <li className='px-4' key={_id}>
               {item}
