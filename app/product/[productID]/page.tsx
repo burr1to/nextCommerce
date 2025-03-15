3ce6a130b1f6371cce0ca491fa6c7d6c68557afb
burrito <singhsanskar240@gmail.com>
2025-03-15 19:00:45 +0545

    Single product page done, only loading and error functionalities left

--- app/product/[productID]/page.tsx
+++ app/product/[productID]/page.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 const SingleProduct = async ({
   params,
@@ -12,8 +13,31 @@ const SingleProduct = async ({
       `https://fakestoreapi.com/products/${productID}`
     );
     const singleProd = response.data;
+    console.log(singleProd);
+    return (
+      <div className='flex flex-col items-center justify-center relative gap-y-4 p-4'>
+        <h2>{singleProd.title}</h2>
+        <p className='capitalize underline'>{singleProd.category}</p>
+        <div className=''>
+          <Image
+            src={singleProd.image}
+            alt='Product'
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className='flex justify-center max-w-[400px]'>
+          <p className='text-center'>{singleProd.description}</p>
+        </div>
+        <div className='text-center'>
+          <p className='text-xl border border-blue-400 rounded-lg px-4 py-1 bg-blue-400 text-white'>
+            ${singleProd.price}
+          </p>
 
-    return <div>Product {singleProd.price}</div>;
+          <p className='m-4'>Rating : {singleProd.rating.rate}</p>
+        </div>
+      </div>
+    );
   } catch (err) {
     console.error(err);
   }
